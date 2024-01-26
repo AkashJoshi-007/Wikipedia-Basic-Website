584f6f335845e0601412b20d3c00c0332fd7871d
AKASH JOSHI <120037667+AkashJoshi-007@users.noreply.github.com>
2024-01-26 17:25:24 +0530

    day35

---
+++ HTML REVISION/Youtube Clone/app.js
@@ -0,0 +1,35 @@
+let a= 10;
+
+console.log(a--); //    10
+console.log(a--); //   9
+console.log(a--); //  8
+console.log(a--); // 7
+
+let b= 20;
+console.log(--b); // 19
+console.log(--b); //   18
+console.log(--b); //     17
+console.log(--b); //       16
+
+let c= 30;
+console.log(c++); //       30
+console.log(c++); //     31
+console.log(c++); //   32
+console.log(c++); // 33
+
+let d= 40;
+
+console.log(++d); // 41
+console.log(++d); //   42
+console.log(++d); //     43
+console.log(++d); //       44
+
+
+
+
+// ++a : it gives o/p by add +1 of corresponding numbers(11)
+// a++ : initiallly gives o/p same as no.(10)
+// --a : it gives o/p by -1 of corresponding numbers.(9)
+// a-- : initiallly gives o/p same as no.(10)
+
+
